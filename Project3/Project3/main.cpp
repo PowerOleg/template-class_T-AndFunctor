453b0048f94e6ad68485b09e776f00afe2866e55
PowerOleg <geraskin.oleg@mail.ru>
2024-06-23 16:10:58 +0300

    task3IsDone.functor_and_operator<<

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -27,13 +27,41 @@ std::ostream& operator<<(std::ostream& os, Get_sum& get_sum)
 }
 
 
+
+class Get_count
+{
+public:
+	Get_count()
+	{
+		this->count = 0;
+	}
+
+	void operator()(int var)
+	{
+		if (var % 3 == 0)
+		{
+			count++;
+		}
+	}
+	friend std::ostream& operator<<(std::ostream& os, Get_count& get_count);
+
+private:
+	int count;
+};
+
+std::ostream& operator<<(std::ostream& os, Get_count& get_count)
+{
+	return os << get_count.count;
+}
+
 int main(int argc, char** argv)
 {
 	auto vec = std::vector<int>{ 4, 1, 3, 6, 25, 54 };
-	Get_sum result = std::for_each(vec.begin(), vec.end(), Get_sum());
-	std::cout << result << std::endl;
+	Get_sum sum = std::for_each(vec.begin(), vec.end(), Get_sum());
+	std::cout << "[OUT]: get_sum() = " << sum << std::endl;
 
-
+	Get_count count = std::for_each(vec.begin(), vec.end(), Get_count());
+	std::cout << "[OUT]: get_count() = " << count << std::endl;
 
 	return 0;
 }
