92c4714d5c031a144d85664af6b41101657aa6c0
PowerOleg <geraskin.oleg@mail.ru>
2024-07-06 11:06:26 +0300

    task2 is fixed

--- Project2/Project2/two_dimensional_array.cpp
+++ Project2/Project2/two_dimensional_array.cpp
@@ -1,12 +1,12 @@
 #include "Two_dimensional_array.h"
 
-template <typename T>
+/*template <typename T>
 const int Two_dimensional_array<T>::size()
 {
 	return this->rows_size;
-}
+}*/
 
-template <typename T>
+/*template <typename T>
 Two_dimensional_array<T>::~Two_dimensional_array()
 {
 	for (size_t i = 0; i < this->rows_size; i++)
@@ -14,9 +14,9 @@ Two_dimensional_array<T>::~Two_dimensional_array()
 		delete[] this->two_dim_array[i];
 	}
 	delete[] this->two_dim_array;
-}
+}*/
 
-template <typename T>
+/*template <typename T>
 Two_dimensional_array<T>::Two_dimensional_array(size_t aCols_size, size_t aRows_size) : rows_size{aRows_size}, cols_size{aCols_size}
 {
 	this->two_dim_array = new int*[aRows_size];
@@ -24,16 +24,16 @@ Two_dimensional_array<T>::Two_dimensional_array(size_t aCols_size, size_t aRows_
 	{
 		this->two_dim_array[i] = new int[aCols_size];
 	}
-}
+}*/
 
-template <typename T>
+/*template <typename T>
 T* Two_dimensional_array<T>::operator[](size_t index)
 {
 	return this->two_dim_array[index];
-}
+}*/
 
-template <typename T>
+/*template <typename T>
 T Two_dimensional_array<T>::operator[](size_t index) const
 {
 	return *this->two_dim_array[index];
-}
+}*/
--- Project2/Project2/two_dimensional_array.h
+++ Project2/Project2/two_dimensional_array.h
@@ -4,11 +4,35 @@ template<typename T>
 class Two_dimensional_array
 {
 public:
-	Two_dimensional_array(size_t cols_size, size_t rows_size);
-	const int size();
-	T* operator[](size_t index);
-	T operator[](size_t index) const;
-	~Two_dimensional_array();
+	Two_dimensional_array(size_t aCols_size, size_t aRows_size) : rows_size{ aRows_size }, cols_size{ aCols_size }
+	{
+		this->two_dim_array = new int* [aRows_size];
+		for (size_t i = 0; i < aRows_size; i++)
+		{
+			this->two_dim_array[i] = new int[aCols_size];
+		}
+	}
+	const int size()
+	{
+		return this->rows_size;
+	}
+	T* operator[](size_t index)
+	{
+		return this->two_dim_array[index];
+	}
+
+	const T operator[](size_t index) const
+	{
+		return *this->two_dim_array[index];
+	}
+	~Two_dimensional_array()
+	{
+		for (size_t i = 0; i < this->rows_size; i++)
+		{
+			delete[] this->two_dim_array[i];
+		}
+		delete[] this->two_dim_array;
+	}
 private:
 	T** two_dim_array;
 	size_t rows_size;
